b49faa10ef33c6552a7637b5d70151b468e8f082
Ivandlpn <ivanpaznotario@gmail.com>
2025-01-01 23:35:05 +0100

    Update script.js

--- MiPK/script.js
+++ MiPK/script.js
@@ -34,7 +34,7 @@ navigator.geolocation.watchPosition((position) => {
             "./doc/L40Ar.json",
             "./doc/L40Br.json",
           "./doc/L40Cr.json",
-           // "./doc/L42Ar.json",
+           "./doc/L42Ar.json",
             "./doc/L42B.json",
             "./doc/L46.json",
             "./doc/L48.json" // Añade más rutas según sea necesario
