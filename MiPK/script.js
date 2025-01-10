f34d9b4550fb2cd4829c0d5b14cb4263f87bdc50
Ivandlpn <ivanpaznotario@gmail.com>
2025-01-10 13:50:25 +0100

    Update script.js

--- MiPK/script.js
+++ MiPK/script.js
@@ -63,7 +63,7 @@ function calcularYActualizarPK() {
             window.pkMasCercano = calcularPKMasCercano(lat, lon, datosCombinados)[0];
             mostrarPKMasCercano(window.pkMasCercano);
             actualizarPosicionPK(window.pkMasCercano);
-            mostrarMensaje("🔄 PK Actualizado");
+            mostrarMensaje("   🔄 PK Actualizado");
         })
         .catch(error => console.error('Error al combinar datos de los archivos:', error));
 }
@@ -285,7 +285,7 @@ document.getElementById("iconoMas").addEventListener("click", () => {
             window.pkMasCercano = calcularPKMasCercano(lat, lon, datosCombinados)[0];
             mostrarPKMasCercano(window.pkMasCercano);
             actualizarPosicionPK(window.pkMasCercano);
-            mostrarMensaje("🔄 PK Actualizado");
+            mostrarMensaje(" 🔄 PK Actualizado");
         })
         .catch(error => console.error('Error al combinar datos de los archivos:', error));
 });
