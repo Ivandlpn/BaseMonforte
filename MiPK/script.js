51b6cd3ea3ed51340b86ed374763d5a1814f6f27
Ivandlpn <ivanpaznotario@gmail.com>
2025-01-15 21:46:27 +0100

    Update script.js

--- MiPK/script.js
+++ MiPK/script.js
@@ -406,7 +406,7 @@ async function obtenerDatosTiempo(lat, lon) {
 
 function mostrarInfoTiempo(ciudad, lat, lon, datosTiempo) {
     if (datosTiempo) {
-        const iconoUrl = `https://openweathermap.org/img/wn/${datosTiempo.icono}@2x.png`;
+        const iconoUrl = `img/iconos-tiempo/${datosTiempo.icono}.png`;
 
         const popupContent = `
             <div style="text-align: center;">
