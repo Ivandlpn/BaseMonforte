a7d10370f6985a144325c7c4ba3aece715508156
Ivandlpn <ivanpaznotario@gmail.com>
2025-01-15 18:24:49 +0100

    Update script.js

--- MiPK/script.js
+++ MiPK/script.js
@@ -396,7 +396,7 @@ async function obtenerDatosTiempo(ciudad, pais) {
 
 function mostrarInfoTiempo(ciudad, lat, lon, datosTiempo) {
     if (datosTiempo) {
-        const iconoUrl = `img/iconos-tiempo/${datosTiempo.icono}.png`;
+        const iconoUrl = `img/iconos-tiempo/${datosTiempo.icono}.png`; // Asumiendo que la carpeta se llama 'img/iconos-tiempo'
 
         const popupContent = `
             <div style="text-align: center;">
