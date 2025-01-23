bfc2801876f1111e3af129657a48371e671feb16
Ivandlpn <ivanpaznotario@gmail.com>
2025-01-23 20:31:51 +0100

    Update script.js

--- MiPK/script.js
+++ MiPK/script.js
@@ -361,7 +361,7 @@ async function activarCapaMiTramo() {
     // Mostrar mensaje "Cargando mi tramo..."
     const pkElement = document.getElementById("pkCercano");
     const textoOriginalPK = pkElement.innerHTML; // Guarda el texto original para restaurar
-    pkElement.innerHTML = `<span class="texto-buscando-pk">Cargando 📍Mi Tramo...</span>`;
+    pkElement.innerHTML = `<span class="texto-buscando-mitramo">Cargando 📍Mi Tramo...</span>`;
 
     try {
         const datosTrazado = await cargarArchivosJSON(rutasArchivos);
