017386d8a57c79a84cf4d4c4a22b87830c1418f5
Ivandlpn <ivanpaznotario@gmail.com>
2025-02-01 21:42:22 +0100

    Update script.js

--- MiPK/script.js
+++ MiPK/script.js
@@ -762,7 +762,7 @@ async function activarCapaEdificios(layerGroup, tipos) {
                                 onclick="compartirUbicacionEdificio(${puntoCoordenadas.Latitud}, ${puntoCoordenadas.Longitud}, '${elemento.NOMBRE.replace(/'/g, "\\'")}');"
                                 aria-label="Compartir ubicación de ${elemento.NOMBRE}"
                             >
-                                <img src="img/edificios/compartirubi.png" alt="Compartir Ubicación" style="width: 55px; height: 20px;">
+                                <img src="img/edificios/compartirubi.png" alt="Compartir Ubicación" style="width: 75px; height: 35px;">
                             </button>
                         </div>
                     `);
