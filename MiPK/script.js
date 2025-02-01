8fe5637b06fb2868d200f650b9f7589d8e1a4f05
Ivandlpn <ivanpaznotario@gmail.com>
2025-02-01 20:07:02 +0100

    Update script.js

--- MiPK/script.js
+++ MiPK/script.js
@@ -657,7 +657,7 @@ async function activarCapaTiempo() {
   }
 
   // Establecer un nivel de zoom fijo al activar la capa Tiempo
-  mapa.setZoom(15); // Nivel de zoom fijo (puedes ajustarlo)
+  mapa.setZoom(10); // Nivel de zoom fijo (puedes ajustarlo)
 }
 
 function desactivarCapaTiempo() {
